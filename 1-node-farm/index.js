68074fe7f8ddf65d8caaf452c70db67b7bd17c62
Arthur Takeda <arthur.ot@hotmail.com>
2020-05-17 13:23:15 -0300

    read json file only once instead of every time it hits /api

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -31,6 +31,9 @@ const url = require('url');
 ////////////////////////////////////////////////
 // Server
 
+const data = fs.readFileSync(`${__dirname}/final/dev-data/data.json`, 'utf-8')
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
 
     const pathName = req.url
@@ -40,11 +43,8 @@ const server = http.createServer((req, res) => {
     } else if (pathName === '/product') {
         res.end('Product')
     } else if (pathName === '/api') {
-        fs.readFile(`${__dirname}/final/dev-data/data.json`, 'utf-8', (err, data) => {
-            const productData = JSON.parse(data);
-            res.writeHead(200, {'Content-type': 'application/json'})
-            res.end(data)
-        })
+        res.writeHead(200, {'Content-type': 'application/json'})
+        res.end(data)
     } else {
         res.writeHead(404, {
             'Content-type': 'text/html'
