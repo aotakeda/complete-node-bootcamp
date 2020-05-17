20edefa210954718cd8d0c07deeb28079adb507d
Arthur Takeda <arthur.ot@hotmail.com>
2020-05-17 11:44:40 -0300

    learn how to write file in node

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -2,3 +2,7 @@ const fs = require('fs');
 
 const textIn = fs.readFileSync('./final/txt/input.txt', 'utf-8')
 console.log(textIn)
+
+const textOut = `Text from file: ${textIn}.\nCreated on ${Date.now()}.`
+fs.writeFileSync('./final/txt/output.txt', textOut);
+console.log('File written.')
