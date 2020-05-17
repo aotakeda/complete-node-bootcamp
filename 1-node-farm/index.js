72c679c04cd14402c2478f8416d23b65455cefd9
Arthur Takeda <arthur.ot@hotmail.com>
2020-05-17 12:02:46 -0300

    show what happened if there's an error

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -9,6 +9,8 @@ console.log('File written.')
 
 // Non-blocking, async way
 fs.readFile('./final/txt/start.txt', 'utf-8', (err, first_file) => {
+    if (err) return console.log(err);
+    
     fs.readFile(`./final/txt/${first_file}.txt`, 'utf-8', (err, second_file) => {
         console.log(second_file)
         fs.readFile('./final/txt/append.txt', 'utf-8', (err, third_file) => {
