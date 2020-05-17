ad6bac058ce716b246b8ff914107b82c742d429b
Arthur Takeda <arthur.ot@hotmail.com>
2020-05-17 13:11:31 -0300

    add paths and 404 response

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -32,8 +32,19 @@ const url = require('url');
 // Server
 
 const server = http.createServer((req, res) => {
-    console.log(req.url)
-    res.end('It works');
+
+    const pathName = req.url
+
+    if (pathName === '/overview' || pathName === '/') {
+        res.end('Overview')
+    } else if (pathName === '/product') {
+        res.end('Product')
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html'
+        })
+        res.end('<h1>Page not found</h1>')
+    }
 })
 
 server.listen(8000, () => {
