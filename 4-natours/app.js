8e885b5d175b2aafdb65e8558f7b406def176c63
Arthur Takeda <arthur.ot@hotmail.com>
2020-05-21 00:39:09 -0300

    add new tour on json file

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.status(200)
@@ -22,6 +23,20 @@ app.get('/api/v1/tours', (req, res) => {
         data: {
             tours: tours
         }
+    })
+})
+
+app.post('/api/v1/tours', (req, res) => {
+    const newId = tours[tours.length -1 ].id + 1;
+    const newTour = Object.assign({ id:newId }, req.body)
+    tours.push(newTour)
+    fs.writeFile(`${__dirname}/starter/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        })
     })
 })
 
