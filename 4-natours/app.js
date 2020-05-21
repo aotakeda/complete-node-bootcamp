d73af9d92d8d9aa1a4e2b3f4f134e04f83bd05f6
Arthur Takeda <arthur.ot@hotmail.com>
2020-05-21 00:27:52 -0300

    read json file on the api

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const express = require('express');
 
 const app = express();
@@ -9,8 +10,19 @@ app.get('/', (req, res) => {
     })
 })
 
-app.post('/', (req, res) => {
-    res.send('Post request')
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/starter/dev-data/data/tours-simple.json`)
+)
+    
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours: tours
+        }
+    })
 })
 
 const port = 3000;
